b120f5a0314f8d106c72d1224229646c15eb3cd5
swodd <swodd@mail.ru>
2017-04-23 21:40:24 +0300

    Connection to DataBase

--- app/src/main/java/com/example/dima/guestbook/MainActivity.java
+++ app/src/main/java/com/example/dima/guestbook/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.dima.guestbook;
 
+import android.app.Activity;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    Button btnAdd, btnRead, btnClear;
+    EditText etName, etEmail;
+
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    //подключаем базу
+    class DBHelper extends SQLiteOpenHelper{
+        //указываем имя и версию базы
+        public DBHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+            //создаем таблицу с полями
+            db.execSQL("create table mytable ("
+                + "id integer primary key autoincrement,"
+                + "name text,"
+                + "email text" + ");");
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
+    }
+
+
 }
